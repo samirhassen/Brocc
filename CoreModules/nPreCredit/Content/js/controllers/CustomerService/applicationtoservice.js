ba1fe9e9143c3f9d23b8fdd0320e0430ea750c37
Samir H. <samir.hassen@brocc.se>
2025-04-16 15:38:49 +0200

    working build

--- CoreModules/nPreCredit/Content/js/controllers/CustomerService/applicationtoservice.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('app', ['ntech.forms']);
-app.controller('ctr', ['$scope', '$http', '$q', function ($scope, $http, $q) {
-        $scope.hit = initialData;
-        window.scope = $scope;
-    }]);
